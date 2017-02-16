c27e61cfe82c8a169bdeaf9faf0d38c148eeacba
slavam1 <mwm1955@gmail.com>
2017-02-16 08:37:13 +0000

    New user created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Новый пользователь успешно создан"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:login, :first_name, :last_name, :middle_name, :position, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
+  validates :login, presence: true, length: { maximum: 50, minimum: 4 }, uniqueness: { case_sensitive: false }
   validates :last_name, presence: true, length: { maximum: 50 }
   validates :first_name, length: { maximum: 50 }
   validates :middle_name, length: { maximum: 50 }
-  validates :login, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 4 }
 end
